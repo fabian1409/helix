f1c5bee9fd61f99a04aa9e56e9bb8236340390de
Fabian Gruber <fabian.gruber@student.tugraz.at>
2024-10-12 11:28:33 +0200

    replaced vec with tree, fixed some commands

--- helix-view/src/file_tree.rs
+++ helix-view/src/file_tree.rs
@@ -1,6 +1,6 @@
-use std::cmp::Ordering;
 use std::fs::DirEntry;
 use std::path::PathBuf;
+use std::{cmp::Ordering, path::Path};
 
 use helix_stdx::path::{fold_home_dir, read_dir_sorted};
 
@@ -12,13 +12,42 @@ pub struct FileTreeItem {
     pub path: PathBuf,
     pub is_dir: bool,
     pub is_expanded: bool,
-    pub depth: usize,
+    pub children: Vec<FileTreeItem>,
 }
 
 impl FileTreeItem {
-    pub fn with_depth(mut self, depth: usize) -> Self {
-        self.depth = depth;
-        self
+    pub fn new(name: String, path: PathBuf, is_dir: bool) -> Self {
+        Self {
+            name,
+            path,
+            is_dir,
+            is_expanded: false,
+            children: vec![],
+        }
+    }
+
+    pub fn root(name: String, path: PathBuf, children: Vec<FileTreeItem>) -> Self {
+        Self {
+            name,
+            path,
+            is_dir: true,
+            is_expanded: true,
+            children,
+        }
+    }
+
+    pub fn expand(&mut self) {
+        self.is_expanded = true;
+        let children = read_dir_sorted(&self.path, false)
+            .into_iter()
+            .map(FileTreeItem::from)
+            .collect::<Vec<FileTreeItem>>();
+        self.children = children
+    }
+
+    pub fn collapse(&mut self) {
+        self.is_expanded = false;
+        self.children.clear();
     }
 }
 
@@ -26,13 +55,11 @@ impl From<DirEntry> for FileTreeItem {
     fn from(value: DirEntry) -> Self {
         let meta = value.metadata().expect("can read meta");
         let name = value.file_name();
-        FileTreeItem {
-            name: name.into_string().expect("can conv to string"),
-            path: value.path(),
-            is_dir: meta.is_dir(),
-            is_expanded: false,
-            depth: 1,
-        }
+        Self::new(
+            name.into_string().expect("can conv to string"),
+            value.path(),
+            meta.is_dir(),
+        )
     }
 }
 
@@ -60,7 +87,7 @@ impl PartialOrd for FileTreeItem {
 
 #[derive(Debug)]
 pub struct FileTree {
-    pub items: Vec<FileTreeItem>,
+    pub root: FileTreeItem,
     pub selection: usize,
     pub open: bool,
     pub copied: Option<FileTreeItem>,
@@ -76,24 +103,111 @@ impl FileTree {
     pub fn new() -> Self {
         let cwd = std::env::current_dir().expect("can get cwd");
         let dir = read_dir_sorted(&cwd, false);
-        let mut items = vec![FileTreeItem {
-            name: fold_home_dir(&cwd).to_string_lossy().to_string(),
-            path: cwd,
-            is_dir: true,
-            is_expanded: true,
-            depth: 0,
-        }];
-        items.extend(dir.into_iter().map(FileTreeItem::from).collect::<Vec<_>>());
+        let children = dir.into_iter().map(FileTreeItem::from).collect::<Vec<_>>();
+        let root = FileTreeItem::root(
+            fold_home_dir(&cwd).to_string_lossy().to_string(),
+            cwd,
+            children,
+        );
         Self {
-            items,
+            root,
             selection: 0,
             open: false,
             copied: None,
         }
     }
 
+    fn flatten_(root: &FileTreeItem, depth: usize) -> Vec<(&FileTreeItem, usize)> {
+        let mut items = vec![];
+        if root.children.is_empty() {
+            items.push((root, depth));
+        } else {
+            items.push((root, depth));
+            for child in root.children.iter() {
+                items.extend(Self::flatten_(child, depth + 1))
+            }
+        }
+        items
+    }
+
+    fn selected_mut_<'a>(
+        node: &'a mut FileTreeItem,
+        n: &mut usize,
+    ) -> Option<&'a mut FileTreeItem> {
+        if *n == 0 {
+            return Some(node);
+        }
+        *n -= 1;
+        for child in node.children.iter_mut() {
+            if let Some(result) = Self::selected_mut_(child, n) {
+                return Some(result);
+            }
+        }
+        None
+    }
+
+    fn selected_<'a>(node: &'a FileTreeItem, n: &mut usize) -> Option<&'a FileTreeItem> {
+        if *n == 0 {
+            return Some(node);
+        }
+        *n -= 1;
+        for child in &node.children {
+            if let Some(result) = Self::selected_(child, n) {
+                return Some(result);
+            }
+        }
+        None
+    }
+
+    fn find_with_path_<'a>(
+        node: &'a mut FileTreeItem,
+        path: &Path,
+    ) -> Option<&'a mut FileTreeItem> {
+        if node.path == path {
+            return Some(node);
+        } else {
+            for ch in node.children.iter_mut() {
+                if let Some(parent) = Self::find_with_path_(ch, path) {
+                    return Some(parent);
+                }
+            }
+        }
+        None
+    }
+
+    pub fn flatten(&self) -> Vec<&FileTreeItem> {
+        Self::flatten_(&self.root, 0)
+            .into_iter()
+            .map(|e| e.0)
+            .collect()
+    }
+
+    pub fn flatten_with_depth(&self) -> Vec<(&FileTreeItem, usize)> {
+        Self::flatten_(&self.root, 0)
+    }
+
     pub fn selected(&self) -> Option<&FileTreeItem> {
-        self.items.get(self.selection)
+        let mut n = self.selection;
+        Self::selected_(&self.root, &mut n)
+    }
+
+    pub fn selected_mut(&mut self) -> Option<&mut FileTreeItem> {
+        let mut n = self.selection;
+        Self::selected_mut_(&mut self.root, &mut n)
+    }
+
+    pub fn take_selected(&mut self) -> Option<FileTreeItem> {
+        let selected = self.selected().cloned()?;
+        let parent = self.find_with_path(selected.path.parent().unwrap())?;
+        Some(
+            parent
+                .children
+                .remove(parent.children.iter().position(|c| *c == selected).unwrap()),
+        )
+    }
+
+    pub fn find_with_path(&mut self, path: &Path) -> Option<&mut FileTreeItem> {
+        Self::find_with_path_(&mut self.root, path)
     }
 
     pub fn move_up(&mut self) {
@@ -101,7 +215,8 @@ impl FileTree {
     }
 
     pub fn move_down(&mut self) {
-        if self.selection < self.items.len() - 1 {
+        let len = self.flatten().len();
+        if self.selection < len - 1 {
             self.selection += 1;
         }
     }
@@ -111,59 +226,14 @@ impl FileTree {
     }
 
     pub fn goto_end(&mut self) {
-        self.selection = self.items.len() - 1;
+        let len = self.flatten().len();
+        self.selection = len - 1;
     }
 
-    pub fn expand(&mut self) {
-        let item = self.items.get_mut(self.selection).unwrap();
-        item.is_expanded = true;
-        let items = read_dir_sorted(&item.path, false)
-            .into_iter()
-            .map(|entry| FileTreeItem::from(entry).with_depth(item.depth + 1))
-            .collect::<Vec<FileTreeItem>>();
-        for item in items.into_iter().rev() {
-            self.items.insert(self.selection + 1, item);
-        }
-    }
-
-    pub fn collapse(&mut self) {
-        let item = self.items.get_mut(self.selection).unwrap();
-        item.is_expanded = false;
-        let depth = self.items.get(self.selection).unwrap().depth;
-        let remove_len = self
-            .items
-            .iter()
-            .skip(self.selection + 1)
-            .take_while(|i| i.depth > depth)
-            .count();
-        for _ in 0..remove_len {
-            self.items.remove(self.selection + 1);
-        }
-    }
-
-    pub fn insert_and_adjust(&mut self, item: FileTreeItem) {
-        let index = self
-            .items
-            .iter()
-            .skip(self.selection + 1)
-            .position(|e| e.depth == item.depth && e > &item)
-            .unwrap()
-            + self.selection
-            + 1;
-        self.items.insert(index, item);
-    }
+    pub fn delete_selection(&mut self) {}
 
     pub fn reload(&mut self) {
-        let cwd = std::env::current_dir().unwrap();
-        let dir = read_dir_sorted(&cwd, false);
-        let mut items = vec![FileTreeItem {
-            name: fold_home_dir(&cwd).to_string_lossy().to_string(),
-            path: cwd,
-            is_dir: true,
-            is_expanded: true,
-            depth: 0,
-        }];
-        items.extend(dir.into_iter().map(FileTreeItem::from).collect::<Vec<_>>());
-        self.items = items;
+        *self = Self::new();
+        self.open = true;
     }
 }
