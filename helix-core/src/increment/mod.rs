c5b1c1860a9ee90a700506346036513127941f01
Fabian Gruber <fabian.gruber@student.tugraz.at>
2024-10-25 20:23:33 +0200

    feat: increment for bool values

--- helix-core/src/increment/mod.rs
+++ helix-core/src/increment/mod.rs
@@ -8,3 +8,11 @@ pub fn integer(selected_text: &str, amount: i64) -> Option<String> {
 pub fn date_time(selected_text: &str, amount: i64) -> Option<String> {
     date_time::increment(selected_text, amount)
 }
+
+pub fn bool(selected_text: &str, _amount: i64) -> Option<String> {
+    match selected_text {
+        "true" => Some("false".to_string()),
+        "false" => Some("true".to_string()),
+        _ => None,
+    }
+}
