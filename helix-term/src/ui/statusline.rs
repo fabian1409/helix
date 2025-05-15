63e704a20eb6db0fde45bac25baf8ed5d114d4a6
Rock Boynton <rock.boynton@yahoo.com>
2025-05-15 14:53:02 +0200

    Show the primary selection index on statusline (#12326)
    Co-authored-by: Rock Boynton <rboynton@anduril.com>
    Co-authored-by: Michael Davis <mcarsondavis@gmail.com>

--- helix-term/src/ui/statusline.rs
+++ helix-term/src/ui/statusline.rs
@@ -338,7 +338,11 @@ where
     let count = selection.len();
     write(
         context,
-        format!(" {} sel{} ", count, if count == 1 { "" } else { "s" }).into(),
+        if count == 1 {
+            " 1 sel ".into()
+        } else {
+            format!(" {}/{count} sels ", selection.primary_index() + 1).into()
+        },
     );
 }
 
