f64455d14957c30334bb2bdb4b9fd1cbdf39560e
Fabian Gruber <fabian.gruber5@gmail.com>
2025-06-01 17:09:11 +0200

    feat: add custom command aliases

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -427,6 +427,8 @@ pub struct Config {
     pub rainbow_brackets: bool,
     /// Whether to enable Kitty Keyboard Protocol
     pub kitty_keyboard_protocol: KittyKeyboardProtocolConfig,
+    /// A map of command aliases to a list of builtin commands. Arguments passed to a alias are passed to last command in the list.
+    pub aliases: HashMap<String, Vec<String>>,
 }
 
 #[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize, Clone, Copy)]
@@ -1118,6 +1120,7 @@ impl Default for Config {
             editor_config: true,
             rainbow_brackets: false,
             kitty_keyboard_protocol: Default::default(),
+            aliases: HashMap::new(),
         }
     }
 }
